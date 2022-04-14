46a1d96da154a360d08347f2c3be4d9543b6d480
Constantin <konstantindanilov19@hotmail.com>
2022-04-14 23:33:19 +0300

    worked with react

---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+const Profile = () => {
+  return <div className='content'>
+  <div>
+  <img src="https://picfiles.alphacoders.com/297/297577.jpg" alt="" />
+  </div>
+  <div className='elephant'>
+  <img src="https://avatars.mds.yandex.net/i?id=2a0000017a0ec4ac39b18cf0fc6545025f63-3451291-images-thumbs&n=13" alt="" />
+  </div>
+  <div>My Posts</div>
+  <div>New Post</div>
+</div>
+}
+export default Profile;
