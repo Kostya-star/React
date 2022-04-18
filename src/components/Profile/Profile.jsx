3ad22c8c6ac8e104ee7c0c708a0aa538b6202122
Constantin <konstantindanilov19@hotmail.com>
2022-04-18 12:43:44 +0300

    worked with react: created another page named Dialogs. Since it is a changable page just like Profile, I created mutual parameters for the two and passed them into App.css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import MyPosts from './MyPosts/MyPosts'
 import ProfileCSS from './Profile.module.css'
 
 const Profile = () => {
-  return <div className={ProfileCSS.content}>
+  return <div>
   <div>
   <img src="https://picfiles.alphacoders.com/297/297577.jpg" alt="" />
   </div>
